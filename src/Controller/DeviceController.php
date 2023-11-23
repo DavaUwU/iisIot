d9cbb38a633dd869571a1f0b94451447dd650ede
DavaUwU <xzahal03@stud.fit.vutbr.cz>
2023-11-23 19:22:06 +0100

    New Device Form

---
+++ src/Controller/DeviceController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Device;
+use App\Form\DeviceFormType;
+use App\Repository\AccountRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\DeviceRepository;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+class DeviceController extends AbstractController
+{
+    private $deviceRepository;
+    private $em;
+    private $accountRepository;
+
+    public function __construct(DeviceRepository $deviceRepository, AccountRepository $accountRepository,
+                                EntityManagerInterface $em)
+    {
+        $this->em = $em;
+        $this->deviceRepository = $deviceRepository;
+        $this->accountRepository = $accountRepository;
+    }
+
+    #[Route('/device', name: 'app_device')]
+    public function index(UserInterface $user): Response
+    {
+        $devices = $this->deviceRepository->findBy(['owner' => $user]);
+
+
+        return $this->render('device/index.html.twig', [
+            'devices' => $devices,
+        ]);
+    }
+
+    #[Route('/device/create', name: 'create_device')]
+    public function create(Request $request, UserInterface $user): Response
+    {
+        $device = new Device();
+        $form = $this->createForm(DeviceFormType::class, $device);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $device->setOwner($this->accountRepository->findOneBy(['username' => $user->getUserIdentifier()]));
+
+            $this->em->persist($device);
+            $this->em->flush();
+
+            return $this->redirectToRoute('app_device');
+        }
+
+        return $this->render('device/create.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+}
---
+++ src/Form/DeviceFormType.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Device;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class DeviceFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('label')
+            ->add('description')
+            ->add('userAlias')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Device::class,
+        ]);
+    }
+}
