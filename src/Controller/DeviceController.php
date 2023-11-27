a6d77e55801c8d83fd66bd69be72c22fc6513aec
Antonín Štoll <antoninstoll@gmail.com>
2023-11-27 18:26:22 +0100

    removed adding devices

--- src/Controller/DeviceController.php
+++ src/Controller/DeviceController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\Device;
 use App\Entity\KPI;
 use App\Entity\Parameter;
+use App\Entity\System;
 use App\Form\AssignFormType;
 use App\Form\DeviceFormType;
 use App\Form\KPIFormType;
@@ -36,19 +37,10 @@ class DeviceController extends AbstractController
     public function index(UserInterface $user): Response
     {
         $devices = $this->deviceRepository->findBy(['owner' => $user]);
-        $assignForms = [];
 
-        foreach ($devices as $device) {
-            $assignForms[$device->getId()] = $this->createForm(AssignFormType::class, $device, [
-                'action' => $this->generateUrl('device_assign_system', ['id' => $device->getId()]),
-                'user' => $user, // Pass the UserInterface object
-                'method' => 'POST',
-            ])->createView();
-        }
 
         return $this->render('device/index.html.twig', [
             'devices' => $devices,
-            'AssignFormType' => $assignForms, // Pass this array to the template
         ]);
     }
 
@@ -128,29 +120,5 @@ class DeviceController extends AbstractController
         ]);
     }
 
-    #[Route('/device/{id}/assign', name: 'device_assign_system', methods: ['POST'])]
-    public function assignSystemToDevice(Request $request, Device $device, EntityManagerInterface $entityManager, UserInterface $user): Response
-    {
-        $form = $this->createForm(AssignFormType::class, $device, [
-        'user' => $user, // Pass the UserInterface object as an option
-    ]);
-        $form->handleRequest($request);
 
-        if ($form->isSubmitted() && $form->isValid()) {
-            $system = $form->get('system')->getData();
-            $device->setSystem($system);
-            $entityManager->persist($device);
-            $entityManager->flush();
-
-            $this->addFlash('success', 'Device assigned to system successfully.');
-
-            return $this->redirectToRoute('app_device');
-        }
-        /*return $this->render('device/index.html.twig', [
-            'AssignFormType' => $form->createView(),
-            'device' => $device,
-            'user' => $user,
-        ]);*/
-        return $this->redirectToRoute('app_device');
-    }
 }
